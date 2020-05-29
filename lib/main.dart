3f78264fa7ae64801bc073f7839f3fcb614fc9e2
Daniel Lopez <dani.lopez@gmail.com>
2020-05-29 16:44:25 +0200

    actualizado con más opciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,14 @@
+import 'package:painter/draw_screen.dart';
 import 'package:flutter/material.dart';
 
-final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
-void main() => runApp(MyApp());
+void main() => runApp(DrawApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class DrawApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dibuja',
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
-      home: Lienzo(),
+      debugShowCheckedModeBanner: false,
+      home: Draw(),
     );
   }
-}
-
-class Lienzo extends StatefulWidget {
-  @override
-  _LienzoState createState() => _LienzoState();
-}
-
-class _LienzoState extends State<Lienzo> {
-  List<Offset> points = <Offset>[];
-
-  @override
-  Widget build (BuildContext context){
-    final Container lienzoArea = Container(
-      margin: EdgeInsets.all(4.0),
-      alignment: Alignment.topLeft,
-      color: Colors.blueGrey[50],
-      child: CustomPaint( painter: Dibuja(points)
-      ),
-    );
-    
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dibuja'),
-      ),
-      body: GestureDetector(
-        onPanUpdate: (DragUpdateDetails details){
-          setState((){
-            RenderBox box =context.findRenderObject();
-            Offset point = box.globalToLocal(details.globalPosition);
-            point = point.translate(0.0, -(AppBar().preferredSize.height));
-
-            points = List.from(points)..add(point);
-          });
-        },
-        onPanEnd: (DragEndDetails details){
-          points.add(null);
-        },
-        child: lienzoArea,
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'limpiar lienzo',
-        backgroundColor: Colors.red,
-        child: Icon(Icons.refresh),
-        onPressed:(){
-          setState(()=>points.clear());
-        },
-      ),
-     );
-  }
-}
-class Dibuja extends CustomPainter{
-  final List<Offset> points;
-  Dibuja(this.points);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    // Define a paint object
-    final paint = Paint()
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = 4.0
-      ..color = Colors.red[400];
-
-    for (int i=0; i< points.length -1 ; i++){
-      if (points[i] != null && points[i + 1] !=null){
-        canvas.drawLine(points[i], points[i + 1], paint);
-      }
-    }
-  }
-
-  @override
-  bool shouldRepaint(Dibuja oldDelegate) {
-   return oldDelegate.points != points;
-  }
-
 }
